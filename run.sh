3e3ba909159e664261ba7d1563399f847d3a8674
Jonathan DeMarks <jmdemarks@spscommerce.com>
2023-12-19 07:27:01 -0600

    Stop using JAVA_HOME in favor of path

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+JAVA_CMD=$(which java)
+if [ -z "$JAVA_CMD" ]; then
+    echo "Could not find java in PATH"
+    exit 1;
+fi
+
 cargo build
-cat test_data.json | $JAVA_HOME/bin/java -Djava.library.path=$(pwd)/target/debug main.java $@
+cat test_data.json | $JAVA_CMD -Djava.library.path=$(pwd)/target/debug main.java $@
